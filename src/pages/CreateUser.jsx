7f7410a3d503429d41190139e17b17a709b5bed3
suraj maheshwari <surajmaheshwari159@gmail.com>
2023-12-31 16:55:08 +0530

    next commit

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -86,88 +86,90 @@ const CreateUser = ({ user }) => {
   };
 
   return (
-    <div className="font-sans flex items-center h-screen overflow-hidden">
-      <div className="flex-1">
-        <img src={User} alt="User" className="" />
-      </div>
-      <div className="flex-1 bg-white h-screen">
-        <div className="flex flex-col gap-5 h-full px-4 py-8 border-l">
-          <h1 className="text-3xl font-bold">
-            {id ? "Update User Profile" : "Create User Profile"}
-          </h1>
-          <form onSubmit={handleSubmit} className="rounded-sm">
-            <div>
-              <label className="text-sm font-medium mb-2 text-gray-500">
-                User Name
-              </label>
-              <input
-                type="text"
-                name="userName"
-                value={userName}
-                className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label className="text-sm font-medium mb-2 text-gray-500">
-                Email
-              </label>
-              <input
-                type="email"
-                name="userEmail"
-                value={userEmail}
-                className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label className="text-sm font-medium mb-2 text-gray-500">
-                Phone
-              </label>
-              <input
-                type="text"
-                name="number"
-                value={number}
-                className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label className="text-sm font-medium mb-2 text-gray-500">
-                Designation
-              </label>
-              <input
-                type="text"
-                name="designation"
-                value={designation}
-                className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-medium text-gray-500">
-                Gender
-              </label>
-              <select
-                value={gender}
-                onChange={handleGender}
-                className="border rounded-md p-2 mb-2 focus:outline-none"
+    <div>
+      <div className="font-sans md:flex md:items-center h-screen sm:overflow-hidden">
+        <div className="sm:flex-1">
+          <img src={User} alt="User" className="" />
+        </div>
+        <div className="sm:flex-1 bg-white min-h-full sm:h-screen">
+          <div className="flex flex-col gap-5 h-full px-4 py-8 border-l">
+            <h1 className="text-3xl font-bold">
+              {id ? "Update User Profile" : "Create User Profile"}
+            </h1>
+            <form onSubmit={handleSubmit} className="rounded-sm">
+              <div>
+                <label className="text-sm font-medium mb-2 text-gray-500">
+                  User Name
+                </label>
+                <input
+                  type="text"
+                  name="userName"
+                  value={userName}
+                  className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label className="text-sm font-medium mb-2 text-gray-500">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="userEmail"
+                  value={userEmail}
+                  className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label className="text-sm font-medium mb-2 text-gray-500">
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="number"
+                  value={number}
+                  className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label className="text-sm font-medium mb-2 text-gray-500">
+                  Designation
+                </label>
+                <input
+                  type="text"
+                  name="designation"
+                  value={designation}
+                  className="p-2 mb-2 border rounded-md w-full text-sm focus:outline-none"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm font-medium text-gray-500">
+                  Gender
+                </label>
+                <select
+                  value={gender}
+                  onChange={handleGender}
+                  className="border rounded-md p-2 mb-2 focus:outline-none"
+                >
+                  <option></option>
+                  {genderCategory.map((option, index) => (
+                    <option value={option || ""} key={index}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <button
+                type="submit"
+                className="bg-gray-800 py-1 px-5 mt-2 text-white rounded-md hover:bg-black"
               >
-                <option></option>
-                {genderCategory.map((option, index) => (
-                  <option value={option || ""} key={index}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="bg-gray-800 py-1 px-5 mt-2 text-white rounded-md hover:bg-black"
-            >
-              {id ? "Update" : "Submit"}
-            </button>
-          </form>
+                {id ? "Update" : "Submit"}
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
